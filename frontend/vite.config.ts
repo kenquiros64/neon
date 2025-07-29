c589e6a74549302cd62fb28afd92a03e53255e3a
kenquiros64 <kquiros@ltvco.com>
2025-07-29 14:16:54 -0600

    Fix prod deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['@emotion/react', '@emotion/styled'],
+  },
+  ssr: {
+    noExternal: ['@emotion/react', '@emotion/styled', '@mui/material'],
+  }
+});
